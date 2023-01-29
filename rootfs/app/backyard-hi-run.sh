d11515072baf4124a1f8e808b900c900e8ead098
bittles <bauern.sc@gmail.com>
2023-01-29 04:07:09 -0500

    exececute permish

--- rootfs/app/backyard-hi-run.sh
+++ rootfs/app/backyard-hi-run.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 set -eu
 
-rm -f gstream-dr-hi.log
+rm -f gstream-by-hi.log
 
-tail --pid $$ -F gstream-dr-hi.log &
+tail --pid $$ -F gstream-by-hi.log &
 
 gst-launch-1.0 rtspsrc protocols=tcp \
-location='rtsp://driveway:kPaWrZG8A9@192.168.21.61:8654/video1_unicast' \
-latency=120 buffer-mode=3 connection-speed=2000 \
+location='rtsp://backyard:wt2g6VXd4V@192.168.21.60:8557/video1_unicast' \
+latency=120 buffer-mode=3 connection-speed=4000 \
 name=t t. ! queue ! \
-capsfilter caps="application/x-rtp,media=video,height=1920,width=1080" ! \
+capsfilter caps="application/x-rtp,media=video,height=1920,width=1080,framerate=24/1" ! \
 queue ! rtph264depay ! h264parse ! queue ! \
 rtspclientsink protocols=tcp latency=0 \
-location=rtsp://192.168.21.40:8554/driveway_hi \
+location=rtsp://192.168.21.40:8554/backyard_hi \
 name=pay t. ! queue ! \
 capsfilter caps="application/x-rtp,media=audio,clock-rate=16000,encoding-name=L16" ! \
 rtpL16depay ! queue ! audioconvert ! queue ! \
 volume volume=1.5 ! audioresample ! \
 opusenc audio-type=2051 bandwidth=-1000 \
 bitrate=64000 frame-size=40 ! queue ! \
-pay. -e > gstream-dr-hi.log
+pay. -e > gstream-by-hi.log
