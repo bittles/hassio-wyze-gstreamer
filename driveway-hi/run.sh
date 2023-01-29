8c8c2acb62e3e3d79c073eeccdf7ccc5e37a6492
bittles <bauern.sc@gmail.com>
2023-01-29 02:53:51 -0500

    initial

---
+++ driveway-hi/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -eu
+
+rm -f gstream.log
+
+tail --pid $$ -F gstream.log &
+
+gst-launch-1.0 rtspsrc protocols=tcp \
+location='rtsp://driveway:kPaWrZG8A9@192.168.21.61:8654/video1_unicast' \
+latency=120 buffer-mode=3 connection-speed=2000 \
+name=t t. ! queue ! \
+capsfilter caps="application/x-rtp,media=video,height=1920,width=1080" ! \
+queue ! rtph264depay ! h264parse ! queue ! \
+rtspclientsink protocols=tcp latency=0 \
+location=rtsp://192.168.21.40:8554/driveway_hi \
+name=pay t. ! queue ! \
+capsfilter caps="application/x-rtp,media=audio,clock-rate=16000,encoding-name=L16" ! \
+rtpL16depay ! queue ! audioconvert ! queue ! \
+volume volume=1.5 ! audioresample ! \
+opusenc audio-type=2051 bandwidth=-1000 \
+bitrate=64000 frame-size=40 ! queue ! \
+pay. -e > gstream.log
